67bad41c6db378a16e319319383b927726394e09
claudioeberle <102823887+claudioeberle@users.noreply.github.com>
2022-09-05 22:24:56 -0300

    Add files via upload

---
+++ clases/Estante.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioC02.clases
+{
+    internal class Estante
+    {
+        private Producto[] productos;
+        private int ubicacionEstante;
+
+        private Estante(int capacidad)
+        {
+            this.productos = new Producto[capacidad];
+        }
+
+        public Estante (int capacidad, int ubicacion) : this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+
+        public Producto[] GetProductos()
+        {
+            return this.productos;
+        }
+
+        public static String MostrarEstante(Estante e)
+        {
+            string retorno = "";
+            string codigo;
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Productos: ");
+            sb.AppendLine(" ");
+
+            foreach (Producto p in e.productos)
+            {
+                sb.AppendLine(Producto.MostrarProducto(p));
+            }
+            sb.AppendLine(" ");
+            sb.Append("Ubicacion: ");
+            sb.Append(e.ubicacionEstante.ToString());
+
+            retorno = sb.ToString();
+            return retorno;
+        }
+
+        public static bool operator ==(Estante e, Producto p)
+        {
+            bool validador = false;
+            int length = e.productos.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (e.productos[i] == (string)p)
+                {
+                    validador = true;
+                    break;
+                }
+            }
+            return validador;
+        }
+
+        public static bool operator !=(Estante e, Producto p)
+        {     
+            return !(e == p);
+        }
+
+        public static bool operator +(Estante e, Producto p)
+        {
+            bool validador = false;
+            int length;
+
+            if(!(e==p))
+            {
+                length = e.productos.Length;
+
+                for (int i = 0; i < length; i++)
+                {
+                    if (e.productos[i] is null)
+                    {
+                        e.productos[i] = p;
+                    }
+                }
+                validador = true;
+            }
+            return validador;
+        }
+
+        public static Estante operator -(Estante e, Producto p)
+        {
+            int length = e.productos.Length;
+
+            if (e == p)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    if(e.productos[i] == (string)p)
+                    {
+                        e.productos[i] = null;
+                    }
+                }
+            }
+            return e;
+        }
+    }
+}
---
+++ clases/Producto.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioC02.clases
+{
+    internal class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        public Producto(string marca, string codigo, float precio)
+        {
+            this.marca = marca;
+            this.codigoDeBarra = codigo;
+            this.precio = precio;
+        }
+
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+
+        public string GetPrecio()
+        {
+            return (this.precio).ToString();
+        }
+
+        public static string MostrarProducto(Producto p)
+        {
+            string producto = "";
+
+            if (p is not null)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                sb.Append("Marca: ");
+                sb.Append(p.marca);
+                sb.Append(",");
+                sb.Append("Precio: ");
+                sb.Append(p.precio.ToString());
+                sb.Append("Código de Barras: ");
+                sb.AppendLine(p.codigoDeBarra);
+
+                producto = sb.ToString();
+            }
+            return producto;
+        }
+
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            bool validador = false;
+
+            if((p1.marca == p2.marca) && ((string)p1 == (string)p2))
+            {
+                validador = true;
+            }
+
+            return validador;
+        }
+
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+
+        public static bool operator ==(Producto p, String marca)
+        {
+            bool validador = false;
+
+            if (p is not null && p.marca == marca)
+            {
+                validador = true;
+            }
+            return validador;
+        }
+
+        public static bool operator !=(Producto p, String marca)
+        {
+            return !(p == marca);
+        }
+
+
+
+
+    }
+}
